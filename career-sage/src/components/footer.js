ae994c5baa3dd88a7a64fb9d0bf56b373dc48d46
Mason Lee <mason.leewp@gmail.com>
2024-11-25 09:23:05 -0800

    Spacing update

--- career-sage/src/components/footer.js
+++ career-sage/src/components/footer.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import '../static/style.css';
+import React from "react";
+import "../static/style.css";
 
 function Footer() {
   return (
     <footer className="w-full bg-neutral-900 text-white py-6 text-center mt-auto rounded-b-lg">
-        <p>© 2024 CareerSage. All Rights Reserved.</p>
-        <div className="flex justify-center gap-4 mt-2">
+      <p>© 2024 CareerSage. All Rights Reserved.</p>
+      <div className="flex justify-center gap-4 mt-2">
         <a href="#">
-            <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-facebook"></i>
         </a>
         <a href="#">
-            <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-twitter"></i>
         </a>
         <a href="#">
-            <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-linkedin"></i>
         </a>
-        </div>
-     </footer>
+      </div>
+    </footer>
   );
 }
 
--- career-sage/src/components/navbar.js
+++ career-sage/src/components/navbar.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../static/style.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../static/style.css";
 
 function Navbar() {
   return (
     <nav>
-    <ul className="flex gap-6">
-      <li>
-        <Link to="/home" className="hover:underline">
-          Home
-        </Link>
-      </li>
-      <li>
-        <a href="#" className="hover:underline">
-          Search Jobs
-        </a>
-      </li>
-      <li>
-        <a href="#" className="hover:underline">
-          Interview Prep
-        </a>
-      </li>
-      <li>
-        <Link to="/contact" className="hover:underline">
-          Contact
-        </Link>
-      </li>
-      <li>
-        <a href="#" className="hover:underline">
-          Sign In
-        </a>
-      </li>
-    </ul>
-  </nav>
+      <ul className="flex gap-6">
+        <li>
+          <Link to="/home" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Search Jobs
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Interview Prep
+          </a>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Sign In
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
